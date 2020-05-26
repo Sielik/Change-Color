92162992e6629a27573b584ff500624853658069
Sielik <kr.sielicki@gmail.com>
2020-05-26 10:40:43 +0200

    First commit

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-/*
-const colorBtn = document.querySelector('.colorBtn');
-const bodyBcg = document.querySelector('body');
-const colors = ['yellow', 'red', 'green', '#3b5998', 'black'];
 
-colorBtn.addEventListener('click', changeColor);
-
-function changeColor(){
-   // bodyBcg.style.backgroundColor = colors[2];
-   let random = Math.floor(Math.random() * colors.length)
-   bodyBcg.style.backgroundColor = colors[random];
-} 
-*/
 const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const colorBtn = document.querySelector('.colorBtn');
 const bodyBcg = document.querySelector('body');
